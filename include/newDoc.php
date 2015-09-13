e18b8eaa8e235d8b95816d3bcae09be82fb84807
sakurayimyim <flying_dm@hotmail.com>
2015-09-13 22:05:02 +0700

    ปรับ CSS + New Doc
    เป็นแบบ Tab

--- include/newDoc.php
+++ include/newDoc.php
@@ -1,13 +1,24 @@
-<div class="content-list">
-  <div class="panel-group" id="accordion1" role="tablist" aria-multiselectable="true">
-  <a role="button" data-toggle="collapse" data-parent="#accordion1" href="#collapseOne1" aria-expanded="true" aria-controls="collapseOne">	
-  <div class="content-header">
-    ข้อมูลสถาบัน
-  </div>
-  </a>
-  <div id="collapseOne1" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-  <div class="panel-body">
-  <div class="content-body">
+<div class="content-list" style="background:none !important;">
+<ul id="newDocument" class="nav nav-tabs">
+    <li role="presentation" class="active">
+        <a href="#pernelinf" id="pernelinf-tab" role="tab" data-toggle="tab" aria-controls="pernelinf" aria-expanded="true">
+         ข้อมูลทั่วไป
+        </a>
+    </li>
+    <li role="presentation">
+        <a href="#perneldetail" id="perneldetail-tab" role="tab" data-toggle="tab" aria-controls="perneldetail">
+        รายละเอียดงาน
+        </a>
+    </li>
+    <li role="presentation">
+        <a href="#pernelfee" id="pernelfee-tab" role="tab" data-toggle="tab" aria-controls="pernelfee">
+        อัตราค่าธรรมเนียม
+        </a>
+    </li>
+</ul>
+
+<div role="tabpanel" id="pernelinf" class="tab-pane fade active in" aria-labelledby="pernelinf-tab">
+<div class="incontent">	
 	<div class="info">
 		<div class="info_left">
 			<div class="label_left"><font color="red">*</font> ลำดับ :</div>
@@ -88,223 +99,19 @@
 		</div>
 	</div>
 	<div class="clean"></div>
-   </div>
-   </div>
-   </div>
+</div>
 </div>
 </div>
 
-<div class="clean mt-57"></div>
-<div class="content-list">
-  <div class="panel-group" id="accordion2" role="tablist" aria-multiselectable="true">
-  <a role="button" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne2" aria-expanded="true" aria-controls="collapseOne">	
-  <div class="content-header">
-    ข้อมูลการรับเรื่อง ยื่นเรื่อง
-  </div>
-  </a>
-  <div id="collapseOne2" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-  <div class="panel-body">
-  <div class="content-body">
-	<div class="info">
-		<div class="info_left">
-			<div class="label_left">วันที่รับเรื่อง :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">วันครบกำหนดส่งงาน :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">วันที่ยื่นกรม ฯ :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">วันกำหนดนัดตรวจ :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">วันนัดเจ้าหน้าที่จริง :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">วันส่งเอกสารให้สถาบัน :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">วันรับเอกสารคืนสถาบัน :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">สถานะปัจจุบัน :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">เจ้าหน้าที่รับงาน :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">ผู้ตรวจ :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">วันออกโรงงานพี่นนท์ :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">ยื่นเรื่อง :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">วันที่ยื่นเรื่อง :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">ตรวจสอบงานผลิตแล้ว :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">ออกโรงงานกับ จนท.กรม :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">จำนวนเครื่อง :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-	</div>
-	<div class="clean"></div>
-   </div>
-   </div>
-   </div>
-</div>
-</div>
+<!--****************Script ***************************-->
+<script type="text/javascript">
+$('#newDocument a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+  console.log($(this))
+})
+</script>
 
-<div class="clean mt-57"></div>
-<div class="content-list">
-  <div class="panel-group" id="accordion3" role="tablist" aria-multiselectable="true">
-  <a role="button" data-toggle="collapse" data-parent="#accordion3" href="#collapseOne3" aria-expanded="true" aria-controls="collapseOne">	
-  <div class="content-header">
-    ข้อมูลค่าธรรมเนียม
-  </div>
-  </a>
-  <div id="collapseOne3" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-  <div class="panel-body">
-  <div class="content-body">
-	<div class="info">
-		<div class="info_left">
-			<div class="label_left">ค่าธรรมเนียมประมาณ :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">ค่าธรรมเนียม :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">วันที่ขอเบิกค่าธรรมเนียม :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">วันที่รับใบเสร็จค่าธรรมเนียม :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">เลขที่ใบเสร็จรับเงินค่าธรรมเนียม :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-	</div>
-	<div class="clean"></div>
-   </div>
-   </div>
-   </div>
-</div>
-</div>
 
-<div class="clean mt-57"></div>
-<div class="content-list">
-  <div class="panel-group" id="accordion4" role="tablist" aria-multiselectable="true">
-  <a role="button" data-toggle="collapse" data-parent="#accordion4" href="#collapseOne4" aria-expanded="true" aria-controls="collapseOne">	
-  <div class="content-header">
-    ข้อมูลเล่มทะเบียน
-  </div>
-  </a>
-  <div id="collapseOne4" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-  <div class="panel-body">
-  <div class="content-body">
-	<div class="info">
-		<div class="info_left">
-			<div class="label_left">วันที่ในเล่มทะเบียน :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">เลขเล่มทะเบียน :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">ลำดับที่ในฐานข้อมูล :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">วันที่รับเล่มเข้า บจ. :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">เวันที่ส่งเล่ม :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">แผ่นป้ายทะเบียนเลขที่ :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">วันติดแผ่นป้ายทะเบียน :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-	</div>
-	<div class="clean"></div>
-    </div>
-   </div>
-   </div>
-</div>
-</div> 
 
-<div class="clean mt-57"></div>
-<div class="content-list">
-  <div class="panel-group" id="accordion5" role="tablist" aria-multiselectable="true">
-  <a role="button" data-toggle="collapse" data-parent="#accordion5" href="#collapseOne5" aria-expanded="true" aria-controls="collapseOne">	
-  <div class="content-header">
-    ข้อมูลใบเสนอราคาและใบวางบิล
-  </div>
-  </a>
-  <div id="collapseOne5" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-  <div class="panel-body">
-  <div class="content-body">
-	<div class="info">
-		<div class="info_left">
-			<div class="label_left">วันที่ส่งใบวางบิล/ใบเสนอราคา :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">เลขที่ใบวางบิล/ ใบเสนอราคา:</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">วันที่ส่งใบเสร็จ :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">เลขที่ใบเสร็จ :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-	</div>
-	<div class="clean"></div>
-   </div>
-   </div>
-   </div>
-</div>
-</div> 
 
-<div class="clean mt-57"></div>
-<div class="content-list">
-  <div class="content-body">
-	<input type="button" class="btn btn-success" value="บันทึก"></div>
-  </div>
-</div>
