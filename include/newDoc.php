2f5f83641557141717ea586c7a560b974e2f1991
sakurayimyim <flying_dm@hotmail.com>
2015-09-14 22:14:44 +0700

    ทำหน้าจอ New Document
    ปรับ CSS ของฟอร์มเอกสาร และทำหน้าจอกรอกเอกสาร

--- include/newDoc.php
+++ include/newDoc.php
@@ -6,7 +6,7 @@
         </a>
     </li>
     <li role="presentation">
-        <a href="#perneldetail" id="perneldetail-tab" role="tab" data-toggle="tab" aria-controls="perneldetail">
+        <a href="#perneldetail" id="perneldetail-tab" role="tab" data-toggle="tab" aria-controls="perneldetail" >
         รายละเอียดงาน
         </a>
     </li>
@@ -16,92 +16,368 @@
         </a>
     </li>
 </ul>
-
+ <div class="tab-content">
 <div role="tabpanel" id="pernelinf" class="tab-pane fade active in" aria-labelledby="pernelinf-tab">
 <div class="incontent">	
 	<div class="info">
 		<div class="info_left">
-			<div class="label_left"><font color="red">*</font> ลำดับ :</div>
+			<div class="label_left"><font color="red">*</font> ลำดับที่ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w120" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left"><font color="red">*</font> รหัสงานบริษัท :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left"><font color="red">*</font> APP :</div>
 			<div class="label_right"><input type="text" class="form-control" ></div>
 		</div>
 		<div class="info_left">
-			<div class="label_left"><font color="red">*</font> เลขที่ออก :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
+			<div class="label_left"><font color="red">*</font> ประเภทนิติกรรม :</div>
+			<div class="label_right">
+				<select class="form-control">
+					<option value="0">กรุณาเลือกข้อมูล</option>
+					<option value="1"></option>
+				</select>
+			</div>
 		</div>
 		<div class="info_left">
-			<div class="label_left">APP :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left"><font color="red">*</font> ทรัพย์ :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left"><font color="red">*</font> สถาบัน :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
+			<div class="label_left"><font color="red">*</font> ชื่อสถาบันการเงิน / ลูกค้า :</div>
+			<div class="label_right">
+				<select class="form-control">
+					<option value="0">กรุณาเลือกข้อมูล</option>
+					<option value="1"></option>
+				</select>
+			</div>
 		</div>
 		<div class="clean"></div>
 		<div class="info_left">
-			<div class="label_left"><font color="red">*</font> ชื่อลูกค้าสถาบัน :</div>
+			<div class="label_left"><font color="red">*</font> เลขที่สัญญาสถาบัน / ลูกค้า :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ชื่อลูกค้าสถาบัน / ลูกค้า :</div>
 			<div class="label_right"><input type="text" class="form-control" ></div>
 		</div>
 		<div class="info_left">
-			<div class="label_left">ผู้ติดต่อ :</div>
+			<div class="label_left">เลขที่ทะเบียนการค้า :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">เลขที่ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ตรอก / ซอย :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ถนน :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">คลอง / แม่น้ำ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">หมู่ที่ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ตำบล :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">อำเภอ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">จังหวัด :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ชื่อผู้ติดต่อ (1) :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">เบอร์โทรศัพท์ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">แฟกซ์ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">อีเมล์ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">เว็บไซต์ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ชื่อผู้ติดต่อ (2) :</div>
 			<div class="label_right"><input type="text" class="form-control" ></div>
 		</div>
 		<div class="info_left">
 			<div class="label_left">เบอร์โทรศัพท์ :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
 		</div>
 		<div class="info_left">
 			<div class="label_left">แฟกซ์ :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">เว็บไซต์ :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
 		</div>
 		<div class="info_left">
 			<div class="label_left">อีเมล์ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">เว็บไซต์ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ชื่อผู้ถือกรรมสิทธิ์เครื่องจักร :</div>
 			<div class="label_right"><input type="text" class="form-control" ></div>
 		</div>
 		<div class="info_left">
-			<div class="label_left">ที่อยู่ลูกค้า :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
+			<div class="label_left">เลขที่ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
 		</div>
 		<div class="info_left">
-			<div class="label_left">จังหวัด :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
+			<div class="label_left">ตรอก / ซอย :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ถนน :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">คลอง / แม่น้ำ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">หมู่ที่ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ตำบล :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
 		</div>
 		<div class="info_left">
 			<div class="label_left">อำเภอ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">จังหวัด :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ชื่อสถานที่ตั้งกรรมสิทธิ์เครื่องจักร :</div>
 			<div class="label_right"><input type="text" class="form-control" ></div>
 		</div>
 		<div class="info_left">
-			<div class="label_left">ทะเบียนโรงงาน :</div>
+			<div class="label_left">พิกัดละติจูด :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">พิกัดลองจิจูด :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">เลขที่ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ตรอก / ซอย :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ถนน :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">คลอง / แม่น้ำ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">หมู่ที่ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ตำบล :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">อำเภอ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">จังหวัด :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ใบอนุญาตประกอบกิจการเลขที่ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">ประเภทอุตสาหกรรมตาม พรบ. โรงงาน / สจก :</div>
 			<div class="label_right"><input type="text" class="form-control" ></div>
 		</div>
 		<div class="info_left">
-			<div class="label_left">ประเภทกิจการ :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">จำนวน :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">รายละเอียด :</div>
-			<div class="label_right"><textarea class="form-control" rows="3"></textarea></div>
-		</div>
-		<div class="info_left">
-			<div class="label_left">เลขที่สัญญาสถานะบัน :</div>
-			<div class="label_right"><input type="text" class="form-control" ></div>
+			<div class="label_left">จำนวนเครื่องจักรที่ทำนิติกรรม (เครื่อง) :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
 		</div>
 	</div>
 	<div class="clean"></div>
 </div>
 </div>
+
+<!--*************************** Sheet 2 Start *************************-->
+<div role="tabpanel" id="perneldetail" class="tab-pane fade" aria-labelledby="perneldetail-tab">
+<div class="incontent">	
+	<div class="info">
+		<div class="info_left">
+			<div class="label_left"><font color="red">*</font> วดป. รับงานและออกรหัสงานบริษัท :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">วดป. ครบกำหนดส่งงานสถาบัน / ลูกค้า :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">วดป. ออกสำรวจเครื่องจักร / ทำแผนผังที่ตั้งเครื่องจักร :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">วดป. วิศวกรบริษัทฯ ตรวจแบบ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">วดป. ที่ยื่นเรื่องกับ สนก. :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">วดป. จนท. สนก.นัดตรวจโรงงาน :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">วดป. ติดแผ่นป้ายทะเบียนเครื่องจักร :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left"><font color="red">*</font> จนท. สนก. ผู้รับผิดชอบงาน :</div>
+			<div class="label_right">
+				<select class="form-control ">
+					<option value="0">กรุณาเลือกข้อมูล</option>
+					<option value="1"></option>
+				</select>
+			</div>
+		</div>
+		<div class="info_left">
+			<div class="label_left"><font color="red">*</font> หัวหน้า สนก. ผู้รับผิดชอบงาน :</div>
+			<div class="label_right">
+				<select class="form-control ">
+					<option value="0">กรุณาเลือกข้อมูล</option>
+					<option value="1"></option>
+				</select>
+			</div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">สถานะปัจจุบัน :</div>
+			<div class="label_right">
+				<select class="form-control">
+					<option value="0">กรุณาเลือกข้อมูล</option>
+					<option value="1"></option>
+				</select>
+			</div>
+		</div>
+		<div class="clean"></div>
+		<div class="info_left">
+			<div class="label_left">วดป. สถานะปัจจุบัน :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">รายละเอียดงานติดปัญหา :</div>
+			<div class="label_right">
+				<select class="form-control">
+					<option value="0">กรุณาเลือกข้อมูล</option>
+					<option value="1"></option>
+				</select>
+			</div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">แนวทางการแก้ไข :</div>
+			<div class="label_right">
+				<select class="form-control">
+					<option value="0">กรุณาเลือกข้อมูล</option>
+					<option value="1"></option>
+				</select>
+			</div>
+		</div>
+	</div>
+	<div class="clean"></div>
 </div>
+</div>
+<!--************************************ Sheet 2 End ****************-->
+
+<!--*************************** Sheet 3 Start ***********************-->
+<div role="tabpanel" id="pernelfee" class="tab-pane fade" aria-labelledby="pernelfee-tab">
+<div class="incontent">	
+	<div class="info">
+		<div class="info_left">
+			<div class="label_left">อัตราค่าธรรมเนียมประมาณการ (บาท) :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">วดป. ขอเบิกค่าธรรมเนียมราชการ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">อัตราค่าธรรมเนียมจ่ายจริง (บาท) :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left clean">
+			<div class="label_left">วดป. ที่ออกใบเสร็จค่าธรรมเนียมราชการ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+		    <div class="label_left">เลขที่ใบเสร็จค่าธรรมเนียม :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">วดป. บนเล่มทะเบียนเครื่องจักร:</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">เลขที่บนเล่มทะเบียนเครื่องจักร :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">เลขที่แผ่นป้ายทะเบียนเครื่องจักร :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">วดป. ที่ส่งเล่มทะเบียน :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="clean"></div>
+		<div class="info_left">
+			<div class="label_left">วดป. ที่งานแล้วเสร็จ :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+		<div class="info_left">
+			<div class="label_left">วดป. ที่ยกเลิกงาน :</div>
+			<div class="label_right"><input type="text" class="form-control form-w250" ></div>
+		</div>
+	</div>
+	<div class="clean"></div>
+</div>
+</div>
+<!--*************************************Sheet 3 End ****************-->
+
+</div>
+</div>
+
 
 <!--****************Script ***************************-->
 <script type="text/javascript">
