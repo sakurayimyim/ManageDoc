93c30cc65531cfbe6981b7c95f7ac183a2a603c0
sakurayimyim <flying_dm@hotmail.com>
2015-09-20 16:53:09 +0700

    รูปโปรไฟล์

--- component/header.php
+++ component/header.php
@@ -1,3 +1,22 @@
+<style type="text/css">
+.head_com{
+  float: right;
+  width: 57px;
+  height: 57px;
+  border-left: 1px solid #89B5D8;
+  padding: 16px 16px;
+  cursor: pointer;
+}
+.head_comicon{
+  width: 24px;
+  height: 24px;
+}
+.head_com:hover{
+  background-color: #89B5D8;
+  opacity: 0.5;
+}
+</style>
+
   <div class="menu-header">
   <div class="company-logo">
   	<img src="images/comlogo.png" width="36" height="36">
@@ -5,8 +24,8 @@
   <div class="company-name">
     บริษัท อินทิเกรเทด เอนเตอร์ไพร์ส คอนซัลแตนท์ จำกัด
   </div>
-    <div class="header-bar-name">
-    <img src="images/profile.png" class="mar_r5" width="30" height="30">
-      <?=$_SESSION['FullName']?>
+  <div class="head_com">
+      <div class="head_comicon"><img src="images/profile-male.png" width="24" height="21"></div>
     </div>
   </div>
+
