771a35240bc68b996e94ca676a7ee45afbeb682e
sakurayimyim <flying_dm@hotmail.com>
2015-09-24 22:41:32 +0700

    Not Found

---
+++ include/notFound.php
@@ -0,0 +1,19 @@
+<style>
+.notfound{
+	background:#fff;
+	width:700px;
+	height:239px;
+	position:fixed;
+	top:35%;
+	left:50%;	
+	margin-left:-350px;
+	margin-top:-120px;
+}
+}
+
+</style>
+
+<div class="notfound">
+<img src="../images/NotFound.png">
+	
+</div>
