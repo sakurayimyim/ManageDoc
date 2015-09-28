c1049547b2fafa8a53631479c510d8565a463ede
sakurayimyim <flying_dm@hotmail.com>
2015-09-28 23:03:15 +0700

    active menu

--- component/leftMenu.php
+++ component/leftMenu.php
@@ -22,38 +22,39 @@
   <div class="menu-title">
     เมนูจัดการข้อมูลในระบบ
   </div>
-  <div class="left-menu-head pointer left-img-3" id="ReportDoc">
+  <?php if($_SESSION['MemberType'] == 1){ ?>
+  <div class="left-menu-head pointer left-img-3 <?php if($_GET['page'] == 'reportDoc'){ echo "menu-left-reportDoc-active"; } ?>" id="ReportDoc">
     <div class="pdd-l20 m-head">
         รายงานสรุปเอกสาร
     </div>
   </div>
-  <div class="left-menu-head pointer left-img-3" id="NewDoc">
+  <?php }?>
+  <?php if($_SESSION['MemberType'] == 1 || $_SESSION['MemberType'] == 2){ ?>
+  <div class="left-menu-head pointer left-img-3 <?php if($_GET['page'] == 'newDoc'){ echo "menu-left-newDoc-active"; } ?>" id="NewDoc">
     <div class="pdd-l20 m-head">
         สร้างเอกสาร
     </div>
   </div>
-  <div class="left-menu-head pointer left-img-4" id="ListDoc">
+  <div class="left-menu-head pointer left-img-4 <?php if($_GET['page'] == 'listDoc'){ echo "menu-left-listDoc-active"; } ?>" id="ListDoc">
     <div class="pdd-l20 m-head">
         รายการเอกสารทั้งหมด
     </div>
   </div>
-  <div class="left-menu-head pointer left-img-4" id="ListDoc">
-    <div class="pdd-l20 m-head">
-        รายการเครื่องจักร
-    </div>
-  </div>
+  <?php }?>
+   <?php if($_SESSION['MemberType'] == 1){ ?>
   <div class="left-menu-head pointer left-img-1 <?php if($_GET['page'] == 'newMember'){ echo "menu-left-newMem-active"; } ?>" id="NewMember">
   <div class="menu-left-addmem"></div>
     <div class="pdd-l20 m-head">
         เพิ่มสมาชิก
     </div>
   </div>
-  <div class="left-menu-head pointer left-img-2" id="ListMember">
+  <div class="left-menu-head pointer left-img-2 <?php if($_GET['page'] == 'listMember'){ echo "menu-left-listMem-active"; } ?>" id="ListMember">
     <div class="pdd-l20 m-head">
         รายการสมาชิก
     </div>
   </div>
-  <div class="left-menu-head pointer left-img-5" id="ManageProfile">
+   <?php }?>
+  <div class="left-menu-head pointer left-img-5 <?php if($_GET['page'] == 'manageProfile'){ echo "menu-left-manageProfile-active"; } ?>" id="ManageProfile">
     <div class="pdd-l20 m-head">
          จัดการข้อมูลส่วนตัว
     </div>
